67bfacf5be04d19edb73fee2e0d899fa6101f6b3
Pablo Hunter <aceleradora@MacBook-Pro-de-aceleradora.local>
2023-03-28 11:42:02 -0300

    tests

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ mapeandoDados(pkm)
 const pokemon2 = (status) => {
   const id = status.map(identificacao => identificacao.id)
   const nome = status.map(nome => nome.name)
+  const type = status.map(types => types.type.name)
 return { 
     id: id[0] ,
     name: nome[0],
-    types: status[0].types[0].type.name, // Array de string
+    types: '', // Array de string
     abilities: Array, // Array de string
     attributes: {
         hp: Number,
@@ -28,5 +29,9 @@ return {
     ]
     }
 }
- console.log(pokemon2(pkm))
-
+//  console.log(pokemon2(pkm))
+// console.log(pkm.map((tipo) => tipo.types[0].type.name))
+const pokemonType = tipos => {
+    return tipos.map(tipo => tipo.type.name)
+}
+console.log(pokemonType(pkm.types))
