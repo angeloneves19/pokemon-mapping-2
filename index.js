369f6770f49476f430f092b507e27e2984675ac5
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-04-05 14:20:34 -0300

    fizemos-alguns-testes

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-const pkm = require("./api")
+const pkm = require("./api");
 
 const pokemon2 = (status) => {
-  const id = status.map((identificacao) => identificacao.id)
-  const nome = status.map((nome) => nome.name)
-  const type = status.map((tipo) => tipo.types[0].type.name).slice(1)
-  const blaze = status.map((ab) => ab.abilities[0].ability.name).slice(1)
-  const solar = status.map((ab) => ab.abilities[1].ability.name).slice(1)
-  const abilitie = blaze.concat(solar)
+  const id = status.map((identificacao) => identificacao.id);
+  const nome = status.map((nome) => nome.name);
+  const type = status.map((tipo) => tipo.types[0].type.name).slice(1);
+  const blaze = status.map((ab) => ab.abilities[0].ability.name).slice(1);
+  const solar = status.map((ab) => ab.abilities[1].ability.name).slice(1);
+  const abilitie = blaze.concat(solar);
   const atributos = (indexDoAtributo) => {
-    const resultadoFinalDoAtributo = pkm
+    const resultadoFinalDoAtributo = status
       .map((atributos) => atributos.stats[indexDoAtributo].base_stat)
       .slice(1)
-      .pop()
-    return resultadoFinalDoAtributo
-  }
-  const filtragemMovies =  pkm.filter((filtro) =>filtro.moves[0].version_group_details[0].version_group.name === "red-blue" && filtro.moves[0].version_group_details[0].version_group.name === "machine" && filtro.moves[0].version_group_details[0].version_group.name === "tutor" )
-  const moves = (indexNome) => {
-    const nome = filtragemMovies.map((filtro) => filtro.moves[indexNome].move.name).slice(1).pop()
-    return nome
-  }
-  const moves2 = (indexLevel) => {
-    const numero = filtragemMovies.map( (filtro) => filtro.moves[indexLevel].version_group_details[0].level_learned_at ).slice().pop()
-    return numero
-  }
+      .pop();
+    return resultadoFinalDoAtributo;
+  };
 
+  const filtragemMovies = status.filter(
+    (filtro) =>
+      filtro.moves[0].version_group_details[0].version_group.name ===
+        "red-blue" 
+  );
+  const nameMoves = (indexNome) => {
+    const nome = filtragemMovies
+      .map((filtro) => filtro.moves[indexNome].move.name)
+      .slice(1)
+      .pop();
+    return nome;
+  };
+
+  const levelMovies = (indexLevel) => {
+    const numero = filtragemMovies
+      .map(
+        (filtro) =>
+          filtro.moves[indexLevel].version_group_details[0].level_learned_at
+      )
+      .slice()
+      .pop();
+    return numero;
+  };
+  
   return {
     id: id[0],
     name: nome[0],
@@ -37,12 +52,37 @@ const pokemon2 = (status) => {
       specialDefense: atributos(4),
       speed: atributos(5),
     },
-  moves: String,
-}
-}
+    moves: [
+      { name: nameMoves(3), lv:  levelMovies(3)},
+    ]
+  }
 
-//console.log(pokemon2(pkm))
-console.log(pkm.map(a => a.moves.find(b => b.move.name == 'scratch')))
+  };
+
+console.log(pokemon2(pkm));
+
+// const moves = () => {
+ // const nome = filtragemMovies.map((filtroName) => filtroName.moves[3].move.name).slice(1).pop();
+  // console.log(filtroName)
+  //return nome;
+//};
+
+// const moves24 = () => {
+//   const numero = filtragemMoviess.map((filtroLv) => filtroLv.moves[3].version_group_details[0].level_learned_at).slice().pop();
+//     return numero;
+// }
+
+// console.log(moves24())
+//console.log(filtragemMovies)
+
+//console.log(moves())
+
+//const nome = pkm.map((a) => a.moves.find((b) => b.move.name == "scratch"));
+//const test = nome.map((c) =>
+ // c.version_group_details.map((a) => a.level_learned_at)
+//);
+//console.log(test)
+
 //const test1 = pkm.map(ab => ab.abilities[0].ability.name).slice(1)
 //const test2 = pkm.map(ab => ab.abilities[1].ability.name).slice(1)
 //console.log(test1.concat(test2))
