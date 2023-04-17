e3a82bb3654ceb40c8ab17944f143481899c4840
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2023-04-17 11:43:59 -0300

    andamento-nos-testes

--- index.js
+++ index.js
@@ -43,9 +43,12 @@ const pokemon2 = (status) => {
 
 //console.log(pokemon2(pkm));
 
-let movesReadBlue = pkm.map(a => a.moves.map(a => a.version_group_details.map(a => a.version_group.name.map(a =>a == "red-blue"))))
+let versaoRedBlue = pkm.map(test => test.moves.map(a => a.version_group_details.find(a => a.version_group.name == "red-blue" && a.move_learn_method.name !== "machine" && a.move_learn_method.name !== "tutor")))
+console.log(versaoRedBlue.map(tre => tre.map(a => a.move_learn_method.name)))
 
-console.log(movesReadBlue)
+//let movesReadBlue = pkm.map(a => a.moves.map(a => a.move.name))
+
+//console.log(movesReadBlue)
 const movesFiltrados =
   pkm.map((a) =>
     a.moves.map((b) =>
@@ -55,6 +58,15 @@ const movesFiltrados =
     )
   )
 //console.log(movesFiltrados.map(a => a.version_group))
+
+
+
+
+
+
+
+
+
 
 //name
 const tests = (nome) => {
